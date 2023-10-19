038c595dfba1b68539c328a5c401d4123a5f35c2
Tasin007 <mdtasinalam.mta@gmail.com>
2023-10-19 18:55:16 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('TechXpress server is running')
+})
+
+app.listen(port, () => {
+    console.log(`TechXpress Server is running on port: ${port}`)
+})
