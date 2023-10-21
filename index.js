b9ee7ba68950e41cd2d56faddd3ed1f87075c682
Tasin007 <mdtasinalam.mta@gmail.com>
2023-10-21 23:08:53 +0600

    Everything revised

--- index.js
+++ index.js
@@ -56,12 +56,12 @@ async function run() {
     });
 
 
-       app.delete("/myCart/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: id };
-        const result = await cartCollection.deleteOne(query);
-        res.send(result);
-      })
+    app.delete("/myCart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
 
        
     app.get("/addproduct", async (req, res) => {
